27e7925c3d2a8bc04015635aa8674cf09b13bcce
Akira Hanashiro <felipehanashiro@hotmail.com>
2015-09-14 23:03:08 +0000

    e2e tests

--- test/e2e/main.js
+++ test/e2e/main.js
@@ -3,13 +3,18 @@
 describe('The main view', function () {
 
   beforeEach(function () {
-    browser.get('http://localhost:3000');
+    browser.get('http://pokedex-hanashiro.c9.io:8081/#/');
   });
 
-  it('list more than 5 awesome things', function () {
-    element.all(by.repeater('awesomeThing in awesomeThings')).count().then(function(count) {
-      expect(count > 5).toBeTruthy();
+  it('should list all Pokemon', function () {
+    element.all(by.repeater('pokemon in Pokemon.list')).count().then(function(count) {
+      expect(count === 718).toBeTruthy();
     });
+  });
+  
+  it('should select a Pokemon', function () {
+    element.all(by.css('.poke-list-item')).get(0).click();
+    expect( element.all(by.css('#pokemon-id')).get(0).click() ).toContain( "Bulbasaur" );
   });
 
 });
